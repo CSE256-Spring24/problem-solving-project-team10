8348b1f8de2b5a96fcca37ab74d8e574ecb5de2d
Mia Park <mia.p@wustl.edu>
2024-03-31 17:58:34 -0500

    warning message

--- view_baseline.js
+++ view_baseline.js
@@ -354,10 +354,12 @@ $('#adv_perm_inheritance').change(function(){
     else {
         // has just been turned off - pop up dialog with add/remove/cancel
         $(`<div id="add_remove_cancel" title="Security">
-            Warning: if you proceed, inheritable permissions will no longer propagate to this object.<br/>
-            - Click Add to convert and add inherited parent permissions as explicit permissions on this object<br/>
-            - Click Remove to remove inherited parent permissions from this object<br/>
-            - Click Cancel if you do not want to modify inheritance settings at this time.<br/>
+        Warning: </br>
+        </br>
+        Click add to keep inherited permissions,</br>
+         but they will be explicit (changing parent will no longer change this object)</br>
+        </br>
+        Click remove to clear all the effective permissions.
         </div>`).dialog({ // TODO: don't create this dialog on the fly
             modal: true,
             width: 400,
